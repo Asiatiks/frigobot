4059e94e9731aeb8cc005645d9f103f5d8ac2bbe
Matthieu972 <matthieu.mondesir@gmail.com>
2017-12-11 21:57:19 +0100

    [LuisBot]: Fruits & Vegetables intents - AddProduct - .env

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var builder = require('botbuilder');
 var restify = require('restify');
+var env     = require('dotenv');
+env.config();
 
 //create the bot
 var connector = new builder.ChatConnector({
@@ -10,9 +12,13 @@ var bot = new builder.UniversalBot(
     connector,
     function(session)
     { 
-        session.send(`Sorry, I didn't understand '%s'. Type 'help' if you need assistance`, session.message.text);
+        session.endDialog(`Sorry, I didn't understand '%s'. Type 'Help' if you need assistance`);
     }
 );
+
+//Recognizer to use a machine learning LUIS
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
 
 //create the host web server
 var server = restify.createServer();
@@ -24,3 +30,50 @@ server.listen(
         console.log('%s listening to %s', server.name, server.url); 
     }
 );
+
+/**
+ * Intents recognizer
+ */
+bot.dialog('AddProduct', [
+    function(session, args, next){
+        session.send(`Welcome to Frigobot! We are analyzing your message: %s`, session.message.text);        
+
+        var intentResult = args.intent;
+        var fruitEntity = builder.EntityRecognizer.findEntity(intentResult.entities, 'fruit');
+        var vegetableEntity = builder.EntityRecognizer.findEntity(intentResult.entities, 'vegetable');
+        var numberEntity = builder.EntityRecognizer.findEntity(intentResult.entities, 'number');
+
+        //Waterfall Dialog
+        if (fruitEntity)
+        {
+            //fruit entity detected
+            console.log('Fruit => %s', session.message.text);
+            next({ response: fruitEntity.entity });
+        } else if (vegetableEntity){
+            //vegetable entity detected
+            console.log('Vegetable => %s', session.message.text);
+            next({ response: vegetableEntity.entity });
+        } else if (numberEntity){
+            //vegetable entity detected
+            console.log('Number => %s', session.message.text);
+            next({ response: numberEntity.entity });
+        } else {
+            console.log('NONE ENTITY HAS BEEN FOUND');
+        }
+    }
+]).triggerAction({
+    matches: 'AddProduct',
+    onInterrupted: function (session){
+        session.send('Please provide an article');
+    }
+});
+
+bot.dialog('RemoveProduct', [
+]).triggerAction({
+    matches: 'RemoveProduct'
+});
+
+bot.dialog('Help', [
+]).triggerAction({
+    matches: 'Help'
+});
