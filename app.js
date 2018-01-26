3fa66ac351843b952d22ae9b2ef9acaea510539b
LeMayhem <lemay.michael@icloud.com>
2018-01-26 05:47:42 +0100

    [Suggestion]: Base carousel OK

--- app.js
+++ app.js
@@ -268,70 +268,98 @@ bot.dialog('RecipeSuggestion', [
     }
 ])
 
-// Add dialog to handle 'Buy' button click
-bot.dialog('buyButtonClick', [
-    function (session, args, next) {
-        // Get color and optional size from users utterance
-        var utterance = args.intent.matched[0];
-        var color = /(white|gray)/i.exec(utterance);
-        var size = /\b(Extra Large|Large|Medium|Small)\b/i.exec(utterance);
-        if (color) {
-            // Initialize cart item
-            var item = session.dialogData.item = { 
-                product: "classic " + color[0].toLowerCase() + " t-shirt",
-                size: size ? size[0].toLowerCase() : null,
-                price: 25.0,
-                qty: 1
-            };
-            if (!item.size) {
-                // Prompt for size
-                builder.Prompts.choice(session, "What size would you like?", "Small|Medium|Large|Extra Large");
-            } else {
-                //Skip to next waterfall step
-                next();
-            }
-        } else {
-            // Invalid product
-            session.send("I'm sorry... That product wasn't found.").endDialog();
-        }   
-    },
-    function (session, results) {
-        // Save size if prompted
-        var item = session.dialogData.item;
-        if (results.response) {
-            item.size = results.response.entity.toLowerCase();
-        }
-
-        // Add to cart
-        if (!session.userData.cart) {
-            session.userData.cart = [];
-        }
-        session.userData.cart.push(item);
-
-        // Send confirmation to users
-        session.send("A '%(size)s %(product)s' has been added to your cart.", item).endDialog();
-    }
-]).triggerAction({ matches: /(recipe)\s.*shirt/i });
 
 
 function carousselSetup(recipeTitle, recipeUrl, recipeImgUrl, session, response)
 {
-    var msg = new builder.Message(session);
-    msg.attachmentLayout(builder.AttachmentLayout.carousel)
-    msg.attachments([
+    var cards = getCardsAttachments();
+
+    // create reply with Carousel AttachmentLayout
+    var reply = new builder.Message(session)
+        .attachmentLayout(builder.AttachmentLayout.carousel)
+        .attachments(cards);
+
+    session.send(reply);
+};
+
+
+
+
+
+
+
+
+
+
+function getCardsAttachments(session) {
+    return [
         new builder.HeroCard(session)
-            .title(recipeTitle)
+            .title('Azure Storage')
+            .subtitle('Offload the heavy lifting of data center management')
+            .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
             .images([
-                builder.CardImage.create(session, recipeImgUrl)
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/storage/media/storage-introduction/storage-concepts.png')
             ])
-            // .subtitle("This what you currently have on your fridge")
-            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
             .buttons([
-                builder.CardAction.imBack(session, "Go to the recipe", "recipe")
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+            ]),
+
+        new builder.ThumbnailCard(session)
+            .title('DocumentDB')
+            .subtitle('Blazing fast, planet-scale NoSQL')
+            .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+            .images([
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png')
             ])
-    ]);  
-    session.send(msg).endDialog();  
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+            ]),
+
+        new builder.HeroCard(session)
+            .title('Azure Functions')
+            .subtitle('Process events with a serverless code architecture')
+            .text('An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-5daae9212bb433ad0510fbfbff44121ac7c759adc284d7a43d60dbbf2358a07a/images/page/services/functions/01-develop.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+            ]),
+
+        new builder.ThumbnailCard(session)
+            .title('Cognitive Services')
+            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
+            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-68b530dac63f0ccae8466a2610289af04bdc67ee0bfbc2d5e526b8efd10af05a/images/page/services/cognitive-services/cognitive-services.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/cognitive-services/', 'Learn More')
+            ])
+    ];
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 bot.dialog('Help', function (session) {
